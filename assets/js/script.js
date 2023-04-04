dffb4f1a24c87507461fb4372c523fd474794c47
sategie <sategie@yahoo.com>
2023-04-04 14:21:45 +0000

    Edit structure for game functions

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+function displayDivisionQuestion() {
+    
+}
+
+
+
+
+
